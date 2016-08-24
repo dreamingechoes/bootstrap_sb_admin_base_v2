efbebfffdea23a83c3022744b3f703d17116d0b2
Ivan Gonzalez <ivan@simplelogica.net>
2016-08-25 00:27:25 +0200

    Add require for font-awesome-rails on module

--- lib/bootstrap_sb_admin_base_v2.rb
+++ lib/bootstrap_sb_admin_base_v2.rb
@@ -3,6 +3,7 @@ require "bootstrap_sb_admin_base_v2/version"
 module BootstrapSbAdminBaseV2
   module Rails
     class Engine < ::Rails::Engine
+      require "font-awesome-rails"
     end
   end
 end
